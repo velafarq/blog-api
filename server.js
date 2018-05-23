e7c29df56a4cedc346c448c992c724ec26a991cc
velafarq <vela.mrdjen@gmail.com>
2018-05-23 11:37:04 -0500

    Update server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+const blogPostsRouter = require('./blogPostsRouter');
+
+app.use(morgan('common'));
+
+app.use('/blog-posts', blogPostsRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
